8d7ae6fb49ec5920e7fbd56630eced1652646ccf
Brianna Foley <brianna.foley87@gmail.com>
2022-08-16 12:15:06 -0500

    index, show, & view controller actions and views

--- app/controllers/apartments_controller.rb
+++ app/controllers/apartments_controller.rb
@@ -2,4 +2,29 @@ class ApartmentsController < ApplicationController
   def index
     @apartments = Apartment.all
   end
+
+  def show
+    @apartment = Apartment.find(params[:id])
+  end
+
+  def new
+    @apartment = Apartment.new
+  end
+
+  def create
+    @apartment = Apartment.new(apartment_params)
+    @apartment.save
+  end
+
+  private
+
+  def apartment_params
+    params.require(:apartment).permit(
+      :name,
+      :address,
+      :description,
+      :price_per_night,
+      :number_of_guests
+    )
+  end
 end
